f77d80c5c1539e350c339f1fdcc9a7cd0361aab0
ruslanshkhaliev <89189171995@mail.ru>
2025-04-26 01:50:44 +0700

    feat: add tamagui theme config

---
+++ src/styles/themes.ts
@@ -0,0 +1,53 @@
+import { tokens } from '@/styles/tokens';
+
+export const dark = {
+	bgMain: tokens.color.graphite900,
+	bgContent: tokens.color.graphite700,
+	bgCard: tokens.color.graphite800,
+
+	textPrimary: tokens.color.graphite50,
+
+	btnBgSecondary: tokens.color.white12,
+	btnBgFocusSecondary: tokens.color.white8,
+	btnTextSecondary: tokens.color.graphite50,
+	btnBgPrimary: tokens.color.orange500,
+	btnTextPrimary: tokens.color.white,
+
+	outline: tokens.color.orange800,
+
+	tabText: tokens.color.graphite800,
+	tabBg: tokens.color.white16,
+	tabBgActive: tokens.color.white,
+
+	inputBg: tokens.color.graphite600,
+
+	separator1: tokens.color.graphite400,
+	separator2: tokens.color.ink300,
+
+	backgroundColor: tokens.color.graphite900,
+};
+export const light = {
+	bgMain: tokens.color.graphite900,
+	bgContent: tokens.color.graphite700,
+
+	textPrimary: tokens.color.graphite50,
+
+	btnBgSecondary: tokens.color.white12,
+	btnBgFocusSecondary: tokens.color.white8,
+	btnTextSecondary: tokens.color.graphite50,
+	btnBgPrimary: tokens.color.orange500,
+	btnTextPrimary: tokens.color.white,
+
+	outline: tokens.color.orange800,
+
+	tabText: tokens.color.graphite800,
+	tabBg: tokens.color.white16,
+	tabBgActive: tokens.color.white,
+
+	inputBg: tokens.color.graphite600,
+
+	separator1: tokens.color.graphite400,
+	separator2: tokens.color.ink300,
+
+	backgroundColor: tokens.color.graphite900,
+};
--- tamagui.config.ts
+++ tamagui.config.ts
@@ -1,32 +1,20 @@
+import { dark, light } from '@/styles/themes';
+import { tokens } from '@/styles/tokens';
 import { defaultConfig } from '@tamagui/config/v4';
 import { createTamagui } from 'tamagui';
 
-export const light = {
-	background: '$gray0',
-	backgroundHover: '$gray1',
-	backgroundPress: '$gray2',
-	borderColor: '$gray3',
-	color: '$textPrimary',
+export const config = createTamagui({
+	...defaultConfig,
+	tokens,
+	themes: {
+		...defaultConfig.themes,
+		dark,
+		light,
+	},
+});
 
-	primary: '$blue',
-	primaryHover: '$blueHover',
-	text: '$textPrimary',
-	textSecondary: '$textSecondary',
-};
+export type CustomConfig = typeof config;
 
-export const dark = {
-	background: '$gray7',
-	backgroundHover: '$gray6',
-	backgroundPress: '$gray5',
-	borderColor: '$gray4',
-	color: '$textOnDark',
-
-	primary: '$blue',
-	primaryHover: '$blueHover',
-	text: '$textOnDark',
-	textSecondary: '$gray4',
-};
-
-export const config = createTamagui(defaultConfig);
-
-type CustomConfig = typeof config;
+// declare module 'tamagui' {
+// 	interface TamaguiCustomConfig extends CustomConfig {}
+// }
